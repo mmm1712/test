6e164f83e1f72389991cd691387837e76ec923bd
Marina  Zhdanova <marinazhdanova@Marinas-MacBook-Air.local>
2025-09-14 15:42:58 -0700

    movie list

---
+++ movies.py
@@ -0,0 +1,8 @@
+import random
+
+movies = ["Inception", "The Social Network", "Interstellar",
+           "The Imitation Game", "Ex Machina", 
+           "Arrival", "Hidden Figures", 
+          "Blade Runner 2049", "Tenet", "Oppenheimer"]
+
+print (random.choice(movies))
